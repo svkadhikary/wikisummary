12191b697cc020fd079b4c81fb4ebbd767605565
svk <svkadhikary7@gmail.com>
2022-01-06 23:17:44 +0530

    threading changes

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from wikiSearch import WikiSearch
 from webdriver_manager.chrome import ChromeDriverManager
 from mongodbOperations import MongoDBOperations
 import threading
+import concurrent.futures
 
 app = Flask(__name__)
 
@@ -18,27 +19,35 @@ chrome_options.add_argument("--disable-dev-shm-uses")
 chrome_options.add_argument("--no-sandbox")
 
 
-class ThreadClass:
+def scrap(wiki_obj, search_string):
+    global title, body_summary, images_encoded, references
+    record = wiki_obj.wiki_scrapper(search_string)
+    title = record["title"]
+    body_summary = record["summary"]
+    images_encoded = record["images"]
+    references = record["references"]
 
-    def __init__(self, wiki_obj, search_string):
-        self.wiki_obj = wiki_obj
-        self.search_string = search_string
-        self.lock = threading.Lock()
-
-        thread = threading.Thread(target=self.scrap)
-        thread.daemon = True
-        thread.start()
-        thread.join()
-
-    def scrap(self):
-        self.lock.acquire()
-        global title, body_summary, images_encoded, references
-        record = self.wiki_obj.wiki_scrapper(self.search_string)
-        title = record["title"]
-        body_summary = record["summary"]
-        images_encoded = record["images"]
-        references = record["references"]
-        self.lock.release()
+# class ThreadClass:
+#
+#     def __init__(self, wiki_obj, search_string):
+#         self.wiki_obj = wiki_obj
+#         self.search_string = search_string
+#         self.lock = threading.Lock()
+#
+#         thread = threading.Thread(target=self.scrap)
+#         thread.daemon = True
+#         thread.start()
+#         thread.join()
+#
+#     def scrap(self):
+#         self.lock.acquire()
+#         global title, body_summary, images_encoded, references
+#         record = self.wiki_obj.wiki_scrapper(self.search_string)
+#         title = record["title"]
+#         body_summary = record["summary"]
+#         images_encoded = record["images"]
+#         references = record["references"]
+#         self.lock.release()
 
 
 @app.route('/')
@@ -55,13 +64,14 @@ def summary():
 
         wiki_obj = WikiSearch(ChromeDriverManager().install(), chrome_options, db_name)
         try:
-            thread = ThreadClass(wiki_obj, search_string)
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                executor.submit(scrap, [wiki_obj, search_string])
+        # thread = ThreadClass(wiki_obj, search_string)
         except Exception as e:
             raise Exception("Error: \t" + str(e))
 
-
-        # images = os.listdir(os.path.join(app.static_folder, "images"))
-        return render_template('summary.html', heading=title, summary=body_summary, images=images_encoded, references=references)
+        return render_template('summary.html', heading=title, summary=body_summary, images=images_encoded,
+                               references=references)
 
 
 if __name__ == '__main__':
